58f8afb40d9c680fd850aef40fe9c948deddaf23
Luke Patterson <luke.patterson@icloud.com>
2023-02-12 00:55:17 -0600

    Moved project CMake. Specified C++20 standard.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Luke Patterson
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Integer.h"
+#include "Chapter5/Ex5-6.cpp"
+
+void Ex5p6();
+
+int main()
+{
+//	Integer lolol = Integer(1);
+//
+//	lolol.OutputValue();
+//	lolol.SetValue(2);
+//	lolol.OutputValue();
+
+	Ex5p6();
+}
